84f2d5c11a1e42d06c628017cdc7180f9ffb7ce9
cristian.cm <cristian.tgs.7@hotmail.com>
2023-05-29 16:30:45 -0300

    cambio 8

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let password = document.getElementById(password).value;
+console.log(password);
+
+let otropassword = document.getElementById(otropassword)
+console.log(otropassword)
+
+/*  js es igual password = otropassword = si
+esta todo bien
+los password no coinciden*/
